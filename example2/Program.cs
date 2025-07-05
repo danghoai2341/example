62a5cf3247a63603f11d0a50b054fc3ca938dcd2
hoaidang2310 <hoaid111@gmail.com>
2025-07-05 14:35:27 +0700

    add
    add

---
+++ example2/Program.cs
@@ -0,0 +1,12 @@
+﻿// tinh dien tich hinh tron
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ban kinh hinh tron");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double s = Math.PI * r * r;
+      Console.WriteLine("Dien tich la" + s);
+    }
+}
