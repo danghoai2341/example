efdb4ab8460e8022c0946be6173fd07d8c280a6d
hoaidang2310 <hoaid111@gmail.com>
2025-07-09 20:05:52 +0700

    add
    add

---
+++ ExampleCVhinhtron/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ban kinh hinh tron");
+        Double r = Convert.ToDouble(Console.ReadLine());
+        Double area = Math.PI * r * r;
+        Double pre = 2 * Math.PI * r;
+        Console.WriteLine($"Chu vi la {pre}");
+        Console.WriteLine($"Dien tich la {area}");
+    }
+}
+
